3a3c79f56d8890772c6377b8e55581eb278ea7f4
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-23 17:49:14 +0200

    Support src layout in Streamlit entry point

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
+from pathlib import Path
 from runpy import run_module
+import sys
+
+ROOT = Path(__file__).resolve().parent
+SRC = ROOT / "src"
+if str(SRC) not in sys.path:
+    sys.path.append(str(SRC))
 
 if __name__ == "__main__":
     run_module("app", run_name="__main__")
